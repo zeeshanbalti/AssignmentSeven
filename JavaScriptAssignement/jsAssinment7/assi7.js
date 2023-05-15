7ddaec35a8f8c8e75384d756326ddc3dbc19bd39
sa_meer <zeeshanbalti2003@gmail.com>
2023-05-15 20:52:52 +0500

    Assignment Seven

---
+++ JavaScriptAssignement/jsAssinment7/assi7.js
@@ -0,0 +1,73 @@
+           // Question_1
+/*var num = 3.125;
+var num1 = Math.round(num);
+var num2 = Math.floor(num);
+var num3 = Math.ceil(num);
+console.log("Number:",num,"Round:",num1,
+            "Floor:",num2, "Ceil:",num3)*/
+
+   
+            //   Question_2
+
+/*var num = -3.525;
+var num1 = Math.round(num);
+var num2 = Math.floor(num);
+var num3 = Math.ceil(num);
+console.log("Number:",num,"Round:",num1,
+            "Floor:",num2, "Ceil:",num3)*/
+
+        //   Question_3
+
+/*var num = -5;
+var num1 = Math.abs(num);
+console.log(num1)*/
+
+
+    //    Question_4
+
+/* var random = Math.random()*6; 
+ var random = Math.round(random)
+ console.log("Randon Num is:",random)*/
+
+      
+    //    Question_5
+
+/*var num = Math.random()*2;
+var num1 = Math.ceil(num);
+var userInput = prompt("Enter ")
+if (num1 == 1) {
+   console.log("Head is won")
+}else {
+    console.log("Tail is Won")
+}
+console.log(num1)*/
+
+    
+        //    Question_6
+
+/*var randomNum = Math.random()*100;
+randomNum = Math.floor(randomNum)
+console.log("Random Number B/w 1 to 100:",randomNum);*/        
+
+        //  Question_7
+
+/*var user = prompt("Enter Your Weight in kilogram");
+console.log("Your Weight is:",user);*/
+
+
+            //  Question_8
+
+
+/*var sec = Math.random()*10;
+var round = Math.round(sec);
+var userInput = prompt("Enter a numbet b/w 1 to 10");
+if (round == userInput) {
+    console.log("Congratulation your number Match ",
+    "your Number:",userInput," secret Number:",round)
+}else {
+    console.log("Sorry! try again",
+    "Your Number:",userInput," secret Number:",round)
+}*/
+
+
+        //   The End...
